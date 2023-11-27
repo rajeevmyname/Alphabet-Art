2633a995ed3a57894c6be62905c6e07612d7dcee
meen- yaduvanshi <meenuyaduvanshi24@gmail.com>
2023-11-27 16:15:51 +0530

    Flip card on click

--- javaScript.js
+++ javaScript.js
@@ -5,7 +5,20 @@ for (let i = 65; i <= 90; i++) {
 
     let frontDiv = document.createElement("div");
     frontDiv.classList.add("front-div");
-    container.appendChild(frontDiv)
     frontDiv.innerHTML = alphabet;
-   container.appendChild(frontDiv)
+
+    let backDivs = document.createElement("div");
+    backDivs.classList.add("back-div");
+
+    let flipContainer = document.createElement("div")
+    flipContainer.classList.add("card")
+    container.appendChild(flipContainer)
+
+    flipContainer.appendChild(frontDiv);
+    flipContainer.appendChild(backDivs)
+    console.log(flipContainer)
+
+    flipContainer.addEventListener("click", () => {
+        flipContainer.classList.toggle("fliped")
+    })
 }
