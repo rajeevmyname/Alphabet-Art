cd8b61b20d487ccc389383f6a05d77623cb16868
Mahimahto <manisha.tkg08@gmail.com>
2023-11-27 14:38:07 +0530

    Creat Responsive Grid Layout

---
+++ intro.js
@@ -0,0 +1,24 @@
+let container = document.querySelector(".container")
+let boxNames;
+for (let i = 65; i < 91; i++) {
+    boxNames = String.fromCharCode(i);
+
+    let card = document.createElement("div")
+    let cell = document.createElement("div")
+    let nextCell = document.createElement("div")
+
+    cell.setAttribute("class", "box")
+    card.setAttribute("class","card")
+    nextCell.setAttribute("class", "backbox")
+
+    card.appendChild(cell)
+    card.appendChild(nextCell)
+    container.appendChild(card)
+
+    cell.innerHTML = boxNames
+}
+
+
+
+
+
