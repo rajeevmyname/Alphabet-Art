a3446af786810492689b0a42a2f36358a4d1c4ae
Pallavi-0880 <pallavithakur0880@gmail.com>
2023-11-27 14:37:48 +0530

    Create responsive Grid layout

---
+++ cards.js
@@ -0,0 +1,19 @@
+let container = document.querySelector(".container")
+let alphabet;
+for (let i = 65; i < 91; i++) {
+	alphabet = String.fromCharCode(i);
+
+	let newparent = document.createElement("div")
+	newparent.classList.add("card");
+	container.appendChild(newparent);
+
+	let cell = document.createElement("div");
+	cell.classList.add("frontCard");
+	newparent.appendChild(cell);
+	cell.innerHTML = alphabet;
+	
+	
+	let box = document.createElement("div");
+	box.classList.add("backCard");
+	newparent.appendChild(box);
+}
