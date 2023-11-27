db11058f4558187efab6b8112b3f26bdf79d9c0f
meen- yaduvanshi <meenuyaduvanshi24@gmail.com>
2023-11-27 22:38:18 +0530

    Style the front and back of tha card

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,12 @@
 var container = document.querySelector(".container");
 let alphabet;
+
 for (let i = 65; i <= 90; i++) {
+
     alphabet = String.fromCharCode(i);
 
     let frontDiv = document.createElement("div");
-    frontDiv.classList.add("front-div");
+    frontDiv.classList.add("front-div")
     frontDiv.innerHTML = alphabet;
 
     let backDivs = document.createElement("div");
@@ -16,7 +18,6 @@ for (let i = 65; i <= 90; i++) {
 
     flipContainer.appendChild(frontDiv);
     flipContainer.appendChild(backDivs)
-    console.log(flipContainer)
 
     flipContainer.addEventListener("click", () => {
         flipContainer.classList.toggle("fliped")
