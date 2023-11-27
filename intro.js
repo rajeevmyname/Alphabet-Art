ca1e748fff9963419e0ce3787768ea14d379b294
Mahimahto <manisha.tkg08@gmail.com>
2023-11-27 21:44:29 +0530

    Add pattern to the front of the card

--- intro.js
+++ intro.js
@@ -24,13 +24,18 @@ let imagesArray = [
     "/Alphabet-Art/images/t.png",
     "/Alphabet-Art/images/u.png",
     "/Alphabet-Art/images/v.png",
-    "/Alphabet-Art/images/w.png", 
+    "/Alphabet-Art/images/w.png",
     "/Alphabet-Art/images/x.png",
     "/Alphabet-Art/images/y.png",
     "/Alphabet-Art/images/z.png"
 ]
 
-for (let i = 65; i < 91; i++) {
+let pattarn = ["pattarn1", "pattarn2", "pattarn3", "pattarn4", "pattarn5", "pattarn6", "pattarn7", "pattarn8", "pattarn9", "pattarn10", "pattarn11", "pattarn12", "pattarn13"]
+
+for (let i = 65, j = 0; i < 91; i++, j++) {
+    if (j > 12)
+        j = 0;
+
     boxNames = String.fromCharCode(i);
 
     let card = document.createElement("div")
@@ -38,13 +43,18 @@ for (let i = 65; i < 91; i++) {
     let nextCell = document.createElement("div")
     let image = document.createElement("img")
     
-    
+    cell.classList.add(pattarn[j])
+    cell.classList.add("box")
 
-    cell.setAttribute("class", "box")
-    card.setAttribute("class","card")
+    console.log(cell)
+
+
+
+    // cell.setAttribute("class", "box")
+    card.setAttribute("class", "card")
     nextCell.setAttribute("class", "backbox")
-    image.setAttribute("src", imagesArray[i-65])
-    image.setAttribute("class" , "image")
+    image.setAttribute("src", imagesArray[i - 65])
+    image.setAttribute("class", "image")
 
 
     card.appendChild(cell)
@@ -54,8 +64,8 @@ for (let i = 65; i < 91; i++) {
 
 
     cell.innerHTML = boxNames
-   
-    card.addEventListener("click" , () => {
+
+    card.addEventListener("click", () => {
         card.classList.toggle("flipled")
     })
 }
