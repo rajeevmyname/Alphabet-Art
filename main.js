c86ff2aa6deab1c6cb34aef05a21c61527a02a78
meenu <yadav.meenu2023@gmail.com>
2023-11-28 15:38:03 +0530

    Add image to the back of the card

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 let container = document.querySelector(".container");
 let alphabet;
+let imgArray = [
+  "image/a.png", "image/b.png", "image/c.png", "image/d.png", "image/e.png", "image/f.png", "image/g.png",
+  "image/h.png", "image/i.png", "image/j.png", "image/k.png", "image/l.png", "image/m.png", "image/n.png",
+  "image/0.png", "image/p.png", "image/q.png", "image/r.png", "image/s.png", "image/t.png", "image/u.png",
+  "image/v.png", "image/w.png", "image/x.png", "image/y.png", "image/z.png"]
+
 let pattern = ["print1", "print2", "print3", "print4", "print5", "print6",]
 
 for (let i = 65, j = 0; i <= 90; i++, j++) {
@@ -24,5 +30,8 @@ for (let i = 65, j = 0; i <= 90; i++, j++) {
   flipContainer.addEventListener("click", () => {
     flipContainer.classList.toggle("flipped")
   })
-
+  let images = document.createElement("img")
+  images.setAttribute("src", imgArray[i - 65])
+  images.setAttribute("class", "imgClass")
+  backDivs.appendChild(images)
 }
