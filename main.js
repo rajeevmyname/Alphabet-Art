ae4eb94e43a839cc4f8da8bb285f2934ea2b143c
meenu <->
2023-11-28 12:58:07 +0530

    flip card on click

--- main.js
+++ main.js
@@ -6,5 +6,18 @@ for (let i = 65; i <= 90; i++) {
   frontDiv.classList.add("front-div");
   container.appendChild(frontDiv);
   frontDiv.innerHTML = alphabet;
+  let backDivs = document.createElement("div");
+  backDivs.classList.add("back-div");
+
+  let flipContainer = document.createElement("div")
+  flipContainer.classList.add("flip-div")
+  container.appendChild(flipContainer)
+
+  flipContainer.appendChild(frontDiv);
+  flipContainer.appendChild(backDivs)
+
+  flipContainer.addEventListener("click", () => {
+    flipContainer.classList.toggle("flipped")
+  })
 
 }
