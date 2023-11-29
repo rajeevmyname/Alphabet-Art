531441882e675a88216825e05b69c902cd9d8b23
meen- yaduvanshi <meenuyaduvanshi24@gmail.com>
2023-11-29 20:30:58 +0530

    Design card according figma

--- main.js
+++ main.js
@@ -4,22 +4,14 @@ var container = document.querySelector(".container");
 // make a var for store aphabets
 let alphabet;
 
-// Make an imgArray in which all Alphabet images when we will click alphabet then flip card and will show image
-let imgArray = [
-    "images/a.png", "images/b.png", "images/c.png", "images/d.png", "images/e.png", "images/f.png", "images/g.png",
-    "images/h.png", "images/i.png", "images/j.png", "images/k.png", "images/l.png", "images/m.png", "images/n.png",
-    "images/o.png", "images/p.png", "images/q.png", "images/r.png", "images/s.png", "images/t.png", "images/u.png",
-    "images/v.png", "images/w.png", "images/x.png", "images/y.png", "images/z.png"]
-
 // Make a pattern array in which store classes for background styling in front-div
-let pattern = ["print1", "print2", "print3", "print4", "print5", "print6", "print7", "print8", "print9"]
-
+let pattern = ["print1", "print2", "print3", "print4", "print5", "print6"]
 
 //This "For loop" used to making alphabet divs, and changing background design with alphabet divs, and changeing image
 // i var for make alphabets 
 // j variable for pattern array
 for (let i = 65, j = 0; i <= 90; i++, j++) {
-    if (j > 8)
+    if (j > 5)
         j = 0
 
     // Number 65-90 to make alphabet a to z
@@ -58,7 +50,9 @@ for (let i = 65, j = 0; i <= 90; i++, j++) {
 
     // when we will click any alphabet then flip cell and show back image
     let images = document.createElement("img")
-    images.setAttribute("src", imgArray[i - 65])
+
+    // Add imges in the back-div of the cards and we added images by using link from images folder 
+    images.setAttribute("src", `images/${alphabet.toLocaleLowerCase()}.png`)
     images.setAttribute("class", "imgClass")
     backDivs.appendChild(images)
 }
