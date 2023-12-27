6771b845fc9bfbc1996abbe07c6e733aa8253aca
Manisha Kumari <manisha.tkg08@gmail.com>
2023-12-27 10:17:39 +0530

    creat array of object in json file and creat user cards

--- contest/main.js
+++ contest/main.js
@@ -1,3 +1,16 @@
-import data from './main.json' assert{type:'json'}
+import data from './main.json' assert{type: 'json'}
 console.log(data);
 
+for (let i = 0; i < data.length; i++) {
+    let artwork = document.querySelector(".artworks")
+    let identityCard = document.createElement("div")
+    artwork.appendChild(identityCard)
+    identityCard.appendChild
+    let userName = document.createElement("p")
+    let userAge = document.createElement("p")
+    let userProfile = document.createElement("img")
+    userName.innerHTML = data[i].name
+    userAge.innerHTML = data[i].age
+    userProfile.setAttribute("src", data[i].src)
+    identityCard.append(userName,userAge,userProfile)
+}
