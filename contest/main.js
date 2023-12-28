9735f2f6a0f6b288f1b643b51eeeb36bdc37c596
Manisha Kumari <manisha.tkg08@gmail.com>
2023-12-28 11:50:59 +0530

    add aligment in paragraph

--- contest/main.js
+++ contest/main.js
@@ -1,16 +1,39 @@
 import data from './main.json' assert{type: 'json'}
-console.log(data);
 
+let cardContainer = document.createElement("div")
+cardContainer.setAttribute("class", "cardContainer")
 for (let i = 0; i < data.length; i++) {
-    let artwork = document.querySelector(".artworks")
-    let identityCard = document.createElement("div")
-    artwork.appendChild(identityCard)
-    identityCard.appendChild
+    //user-Alphabet-Card
+    let artworkContainer = document.querySelector(".artwork-container")
+    let card = document.createElement("div")
+    cardContainer.appendChild(card)
+    card.setAttribute("class", "card")
+    artworkContainer.appendChild(cardContainer)
+    //user-alphabet-art
+    let alphabetArt = document.createElement("img")
+    alphabetArt.setAttribute("class", "alphabet-art")
+    alphabetArt.setAttribute("src", "https://img.freepik.com/premium-photo/fantasy-monogram-letter_738906-322.jpg?size=626&ext=jpg&ga=GA1.1.1361178089.1689231563&semt=ais")
+    alphabetArt.setAttribute("alt", "alphabetArt")
+    card.appendChild(alphabetArt)
+    //user profile
+    let userProfile = document.createElement("div")
+    userProfile.setAttribute("class", "user-profile")
+    card.appendChild(userProfile)
+    //user-name
     let userName = document.createElement("p")
-    let userAge = document.createElement("p")
-    let userProfile = document.createElement("img")
+    userName.setAttribute("class", "user-name")
     userName.innerHTML = data[i].name
+    //user-age
+    let userAge = document.createElement("p")
     userAge.innerHTML = data[i].age
-    userProfile.setAttribute("src", data[i].src)
-    identityCard.append(userName,userAge,userProfile)
+    userAge.setAttribute("class", "user-age")
+    //user identity                                                                
+    let identity = document.createElement("div")
+    identity.append(userName, userAge)
+    //user-profile-picture
+    let profileImg = document.createElement("img")
+    profileImg.setAttribute("class", "profile-img")
+    profileImg.setAttribute("src", data[i].src)
+    profileImg.setAttribute("alt", "user-profile-picture")
+    userProfile.append(profileImg, identity)
 }
