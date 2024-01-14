9d4dcff7ccc141b0cde126e7e31e83b35f60026e
Rajeev Ranjan Jha <rajeevmyname@gmail.com>
2024-01-14 07:35:28 +0530

    Re-style contest page

--- contest/main.js
+++ contest/main.js
@@ -1,39 +1,39 @@
-import data from './main.json' assert{type: 'json'}
+// import data from './main.json' assert{type: 'json'}
 
-let cardContainer = document.createElement("div")
-cardContainer.setAttribute("class", "cardContainer")
-for (let i = 0; i < data.length; i++) {
-    //user-Alphabet-Card
-    let artworkContainer = document.querySelector(".artwork-container")
-    let card = document.createElement("div")
-    cardContainer.appendChild(card)
-    card.setAttribute("class", "card")
-    artworkContainer.appendChild(cardContainer)
-    //user-alphabet-art
-    let alphabetArt = document.createElement("img")
-    alphabetArt.setAttribute("class", "alphabet-art")
-    alphabetArt.setAttribute("src", "https://img.freepik.com/premium-photo/fantasy-monogram-letter_738906-322.jpg?size=626&ext=jpg&ga=GA1.1.1361178089.1689231563&semt=ais")
-    alphabetArt.setAttribute("alt", "alphabetArt")
-    card.appendChild(alphabetArt)
-    //user profile
-    let userProfile = document.createElement("div")
-    userProfile.setAttribute("class", "user-profile")
-    card.appendChild(userProfile)
-    //user-name
-    let userName = document.createElement("p")
-    userName.setAttribute("class", "user-name")
-    userName.innerHTML = data[i].name
-    //user-age
-    let userAge = document.createElement("p")
-    userAge.innerHTML = data[i].age
-    userAge.setAttribute("class", "user-age")
-    //user identity                                                                
-    let identity = document.createElement("div")
-    identity.append(userName, userAge)
-    //user-profile-picture
-    let profileImg = document.createElement("img")
-    profileImg.setAttribute("class", "profile-img")
-    profileImg.setAttribute("src", data[i].src)
-    profileImg.setAttribute("alt", "user-profile-picture")
-    userProfile.append(profileImg, identity)
-}
+// let cardContainer = document.createElement("div")
+// cardContainer.setAttribute("class", "cardContainer")
+// for (let i = 0; i < data.length; i++) {
+//     //user-Alphabet-Card
+//     let artworkContainer = document.querySelector(".artwork-container")
+//     let card = document.createElement("div")
+//     cardContainer.appendChild(card)
+//     card.setAttribute("class", "card")
+//     artworkContainer.appendChild(cardContainer)
+//     //user-alphabet-art
+//     let alphabetArt = document.createElement("img")
+//     alphabetArt.setAttribute("class", "alphabet-art")
+//     alphabetArt.setAttribute("src", "https://img.freepik.com/premium-photo/fantasy-monogram-letter_738906-322.jpg?size=626&ext=jpg&ga=GA1.1.1361178089.1689231563&semt=ais")
+//     alphabetArt.setAttribute("alt", "alphabetArt")
+//     card.appendChild(alphabetArt)
+//     //user profile
+//     let userProfile = document.createElement("div")
+//     userProfile.setAttribute("class", "user-profile")
+//     card.appendChild(userProfile)
+//     //user-name
+//     let userName = document.createElement("p")
+//     userName.setAttribute("class", "user-name")
+//     userName.innerHTML = data[i].name
+//     //user-age
+//     let userAge = document.createElement("p")
+//     userAge.innerHTML = data[i].age
+//     userAge.setAttribute("class", "user-age")
+//     //user identity                                                                
+//     let identity = document.createElement("div")
+//     identity.append(userName, userAge)
+//     //user-profile-picture
+//     let profileImg = document.createElement("img")
+//     profileImg.setAttribute("class", "profile-img")
+//     profileImg.setAttribute("src", data[i].src)
+//     profileImg.setAttribute("alt", "user-profile-picture")
+//     userProfile.append(profileImg, identity)
+// }
--- header.js
+++ header.js
@@ -1,25 +1,22 @@
-let hearder = document.getElementById("header")
-let contant = `    <header id="header">
-<!-- <Logo/> -->
+let nav = 
+`<!-- <Logo/> -->
 <div class="logo">
     <img src="https://placehold.co/250x100/red/white?text=Logo" alt="Logo">
 </div>
-
 <!-- <NavigationMenu/> -->
 <nav>
     <div>
-        <a href="stickers/index.html"><img src="../assets/navImages/sticker.png" alt="Stickers"></a>
+        <a href="../stickers/index.html"><img src="../assets/navImages/sticker.png" alt="Stickers"></a>
         <p>Stickers</p>
     </div>
     <div>
-        <a href="workshop/index.html"><img src="../assets/navImages/workshop.png" alt="Workshop"></a>
+        <a href="../workshop/index.html"><img src="../assets/navImages/workshop.png" alt="Workshop"></a>
         <p>Workshop</p>
     </div>
     <div>
-        <a href="contest/index.html"><img src="../assets/navImages/contest.png" alt="Contest"></a>
+        <a href="../contest/index.html"><img src="../assets/navImages/contest.png" alt="Contest"></a>
         <p>Contest</p>
     </div>
-</nav>
-</header>`
-
-hearder.innerHTML = contant
+</nav>`
+let header = document.getElementById("header");
+header.innerHTML = nav;
