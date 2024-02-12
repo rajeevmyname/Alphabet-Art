44d5f5847e378ed7242497e64a620d76b18741df
Rajeev Ranjan Jha <rajeevmyname@gmail.com>
2024-02-12 08:10:53 +0530

    Fix bug in navigator share

--- public/main.js
+++ public/main.js
@@ -69,12 +69,14 @@ for(let i=65; i<=90; i++){
     shareIcon.addEventListener("click",() =>{
         let currentUrl = window.location.href
         let url = currentUrl + "?"+`q=${alphabet}`
+
         if (navigator.share) {
             navigator.share({
               title: 'Alphabet-Art',
-            //   url: url,
-              files: [`./assets/alphabet_images/${alphabet}.png`],
-              text: `${alphabet} for ${words[i - 65]} Click to see More Alphabet Art`
+              url: './assets/alphabet_images/${alphabet}.png',
+            //   files: [`./assets/alphabet_images/${alphabet}.png`],
+              text: `*${alphabet}* for *${words[i - 65]}* 
+              Click to see More Alphabet Art`
             })
           } else {
             tooltip.style.visibility = "visible"
